97b891002d41dc25f3921ab0688e0d1969564569
killbane1232 <killbane1232@yandex.ru>
2025-09-22 14:48:23 +0300

    fix duplicate

--- src/main/kotlin/ru/arcam/yggdrasil/telegram/buttons/Menu.kt
+++ src/main/kotlin/ru/arcam/yggdrasil/telegram/buttons/Menu.kt
@@ -63,6 +63,5 @@ abstract class Menu(var chatId: Long, var buttons: List<Button>, val text: Strin
             onResult(result)
         }.start()
         resolver.lastMenuChanged[chatId] = true
-        resolver.bot!!.sendKeyBoard(chatId, resolver.peekMenu(chatId))
     }
 }
